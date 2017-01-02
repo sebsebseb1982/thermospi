28dd60e61e4931f13264b35cc5d4e648682e7eee
Seb <seb@seb.fr>
2017-01-02 13:43:28 +0100

    Premier commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thermospiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the thermospiApp
+ */
+angular.module('thermospiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thermospiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the thermospiApp
+ */
+angular.module('thermospiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
