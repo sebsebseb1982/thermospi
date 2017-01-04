338adc1af5939c5e63965f5e187caa72b0d05cc6
Seb <seb@seb.fr>
2017-01-04 13:09:25 +0100

    Ajout d'ui-router

---
+++ app/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name thermospiApp
+ * @description
+ * # thermospiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('thermospiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource'
+  ])
+  .config(function () {
+
+  });
