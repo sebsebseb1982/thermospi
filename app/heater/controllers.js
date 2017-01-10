af7e2190793b1bd2995475f994b38639795abfdf
sebsebseb <seb@seb.fr>
2017-01-10 23:34:07 +0100

    Controle du chauffage

--- app/heater/controllers.js
+++ app/heater/controllers.js
@@ -12,19 +12,24 @@ angular
   .module('heater.controllers', [])
   .controller('HeaterController', ['$scope', 'SetPoints', function($scope, SetPoints) {
 
+    let ratio = 10;
+
     let updateSetPoint = (sliderId, modelValue, highValue, pointerType) => {
-      alert(modelValue);
+      alert(modelValue / ratio);
     };
 
     $scope.lastSetPoint = SetPoints.getLastSetPoint();
 
     $scope.lastSetPoint.$promise.then((lastSetPoint) => {
       $scope.slider = {
-        value: lastSetPoint.value,
+        value: lastSetPoint.value * ratio,
         options: {
-          floor: 15,
-          ceil: 25,
-          onEnd: updateSetPoint
+          floor: 15 * ratio,
+          ceil: 25 * ratio,
+          onEnd: updateSetPoint,
+          translate: (value, sliderId, label) => {
+            return value / ratio;
+          }
         }
       };
     });
